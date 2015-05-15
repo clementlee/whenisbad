a279181d7c7cd08717f43db98e553c3d036ba500
Clement Lee <clem@princeton.edu>
2015-05-15 03:41:21 -0400

    test of the templating engine

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "html/template"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*"))
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[1:]
+    err := templates.ExecuteTemplate(w, "index", title)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+}
 
 func main() {
-    fmt.Printf("testing go\n")
+    http.HandleFunc("/", handler)
+
+    //handle static files, perhaps better left to nginx in future?
+    fs := http.FileServer(http.Dir("static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    http.ListenAndServe(":8080", nil)
 }
