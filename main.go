c5fe66aa5e3aa37203e26a7e0999e4aeda4505c7
Clement Lee <clem@princeton.edu>
2015-06-11 22:03:33 -0400

    added grid system

--- main.go
+++ main.go
@@ -1,45 +1,44 @@
 package main
 
 import (
-    "os"
-    "strconv"
-    //"fmt"
-    "log"
-    "net/http"
-    "html/template"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 var templates = template.Must(template.ParseGlob("templates/*"))
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[1:]
-    err := templates.ExecuteTemplate(w, "index", title)
+	title := r.URL.Path[1:]
+	err := templates.ExecuteTemplate(w, "index", title)
 
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 
-    //handle static files, perhaps better left to nginx in future?
-    fs := http.FileServer(http.Dir("static"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
+	//handle static files, perhaps better left to nginx in future?
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    //default port, but allow command line argument to override port
-    port := ":8080"
-    if len(os.Args) > 1 {
-        tport := os.Args[1]
-        i, err := strconv.Atoi(tport)
-        if err != nil || i < 0 || i > 65535 {
-            log.Printf("Error: specified port %q is invalid.\n", tport)
-            log.Printf("Defaulting to port 8080.\n")
-        } else {
-            port = ":" + tport
-        }
-    }
+	//default port, but allow command line argument to override port
+	port := ":8080"
+	if len(os.Args) > 1 {
+		tport := os.Args[1]
+		i, err := strconv.Atoi(tport)
+		if err != nil || i < 0 || i > 65535 {
+			log.Printf("Error: specified port %q is invalid.\n", tport)
+			log.Printf("Defaulting to port 8080.\n")
+		} else {
+			port = ":" + tport
+		}
+	}
 
-    log.Fatal(http.ListenAndServe(port, nil))
+	log.Fatal(http.ListenAndServe(port, nil))
 }
